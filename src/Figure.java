2b452f9b01bcab111ef4a5bd3120fe3901425d16
Dan-gem <Dan_dan_gen@mail.ru>
2017-02-15 18:35:08 +0600

    Initial commit

---
+++ src/Figure.java
@@ -0,0 +1,65 @@
+public class Figure {
+    public static void main(String[] args) {
+
+//        for (int a=0; a<10; a++) { //прямоугольник
+//
+//            for (int b=0; b<30; b++) {
+//                System.out.print("*");
+//            }
+//            System.out.print("\n");
+//        }
+//
+//
+//        for (int a=0; a<10; a++) { //
+//
+//            for (int b=0; b<a; b++) {
+//                System.out.print("*");
+//            }
+//            System.out.print("\n");
+//        }
+//
+//
+//        for (int a=0; a<10; a++) { //
+//
+//            for (int b=0; b<a; b++) {
+//
+//                System.out.print("*");
+//            }
+//            System.out.print("\n");
+//        }
+
+
+        if ((x > 0) && (( x & ( x - 1 )) == 0)) {
+            System.out.println ("Число является степенью двойки");
+        } else {
+            System.out.println ("Число не является степенью двойки");
+        }
+
+
+        int i1 = 0, i2 = 0;
+        for (int a = 0; a<14; a++)
+        {
+            for (int b = 0; b < 15; b++)
+            {
+
+                if (b == i1 || b == i2) System.out.print("*");
+                System.out.print(" ");
+            }
+            System.out.print("\n");
+            i2++;
+        }
+        for (int b = 0; b < 15; b++)
+        {
+            System.out.print("*");
+        }
+        System.out.print("");
+    }
+
+
+
+//        System.out.print("*");
+//        System.out.print(" ");
+//        System.out.print("\n");
+
+    }
+
