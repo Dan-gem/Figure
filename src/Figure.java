9a63547a43f492cd07887fb339ca322335131de7
Dan-gem <Dan_dan_gen@mail.ru>
2017-02-16 18:11:55 +0600

    1

--- src/Figure.java
+++ src/Figure.java
@@ -8,7 +8,7 @@ public class Figure {
 //            }
 //            System.out.print("\n");
 //        }
-//
+
 //
 //        for (int a=0; a<10; a++) { //
 //
@@ -29,13 +29,13 @@ public class Figure {
 //        }
 
 
-        if ((x > 0) && (( x & ( x - 1 )) == 0)) {
-            System.out.println ("Число является степенью двойки");
-        } else {
-            System.out.println ("Число не является степенью двойки");
-        }
-
-
+//        if ((x > 0) && (( x & ( x - 1 )) == 0)) {
+//            System.out.println ("Число является степенью двойки");
+//        } else {
+//            System.out.println ("Число не является степенью двойки");
+//        }
+//
+//
         int i1 = 0, i2 = 0;
         for (int a = 0; a<14; a++)
         {
@@ -57,9 +57,30 @@ public class Figure {
 
 
 
+//        for (int a = 1; a < 10; a++) {
+//            if (a == 5) {
+//                System.out.print("*");
+//            } else {
+//                System.out.print(" ");
+//            }
+
+//            for (int b = 0; b < a; b++) {
+//                if ((b%2)==0) {
+//                    System.out.print("*");
+//                } else {
+//                    System.out.print(" ");
+                }
+
+//            for (int b = 0; b < a; b++) {
+//                System.out.print(b % 2 == 0 ? "*" : " ");
+//            }
+//                System.out.print("\n");
+//        }
+//    }
+
+
 //        System.out.print("*");
 //        System.out.print(" ");
 //        System.out.print("\n");
 
-    }
 
