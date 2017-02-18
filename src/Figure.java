f8f1207e00ef56c5a28797c9f3989453d54f0a62
Dan-gen <Dan_dan_gen@mail.ru>
2017-02-19 00:05:29 +0600

    1

--- src/Figure.java
+++ src/Figure.java
@@ -1,86 +1,98 @@
 public class Figure {
     public static void main(String[] args) {
 
-//        for (int a=0; a<10; a++) { //прямоугольник
-//
-//            for (int b=0; b<30; b++) {
-//                System.out.print("*");
-//            }
-//            System.out.print("\n");
-//        }
 
+//        int c=10;
+//        for (int a=0; a<c; a++) { //прямоугольник
 //
-//        for (int a=0; a<10; a++) { //
-//
-//            for (int b=0; b<a; b++) {
-//                System.out.print("*");
-//            }
-//            System.out.print("\n");
-//        }
-//
-//
-//        for (int a=0; a<10; a++) { //
-//
-//            for (int b=0; b<a; b++) {
-//
+//            for (int b=0; b<c; b++) {
 //                System.out.print("*");
 //            }
 //            System.out.print("\n");
 //        }
 
 
-//        if ((x > 0) && (( x & ( x - 1 )) == 0)) {
-//            System.out.println ("Число является степенью двойки");
-//        } else {
-//            System.out.println ("Число не является степенью двойки");
-//        }
-//
-//
-        int i1 = 0, i2 = 0;
-        for (int a = 0; a<14; a++)
-        {
-            for (int b = 0; b < 15; b++)
-            {
+            int c=10;
+        for (int a=0; a<c; a++) { // прямоугольный треугольник
 
-                if (b == i1 || b == i2) System.out.print("*");
-                System.out.print(" ");
+            for (int b=0; b<a; b++) {
+                System.out.print("*");
             }
             System.out.print("\n");
-            i2++;
         }
-        for (int b = 0; b < 15; b++)
-        {
-            System.out.print("*");
+
+
+//        int i1 = 0, i2 = 0; // Прямоугольный треугольник - пустотелый
+//        for (int a = 0; a < 14; a++) {
+//            for (int b = 0; b < 15; b++) {
+//
+//                if (b == i1 || b == i2) System.out.print("*");
+//                System.out.print(" ");
+//            }
+//            System.out.print("\n");
+//            i2++;
+//        }
+//        for (int b = 0; b < 15; b++) {
+//            System.out.print("*");
+//        }
+//        System.out.print("");
+//    }
+
+
+//        int c = 11; // Ромб
+//        int h = 0;
+//        for (int i = 0; i < c; i++) {
+//            if (i < c / 2) {
+//                h = c / 2 + i;
+//            } else {
+//                h = i - c / 2;
+//            }
+//            for (int k = 0; k < c; k++) {
+//                if (k == h || k == c - h - 1) {
+//                    System.out.print("*");
+//                } else {
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.print("\n");
+//        }
+
+
+//        int count = 10; // квадрат с диагоналями внутри
+//        for (int i = 0; i < count; i++) {
+//            for (int k = 0; k < count; k++) {
+//                if (i == count - 1 || i == k || k == count - 1 || k == 0 || i == 0 || k == count - i - 1) {
+//                    System.out.print("*");
+//                } else {
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.print("\n");
+//        }
+
+
+
+
+//        int a = 11; // равносторонний треугольник
+//        for(int i = 0; i < a; i++){
+//            if(i < a/2)
+//                continue;{
+//                for(int k = 0; k < a; k++){
+//                    if(i == a - 1 || k == a - 1 - i || k == i )
+//                    System.out.print("*");
+//                    else
+//                    System.out.print(" ");
+//                }
+//                System.out.print("\n");
+//            }
+//        }
+
+
+
         }
-        System.out.print("");
     }
 
 
 
-//        for (int a = 1; a < 10; a++) {
-//            if (a == 5) {
-//                System.out.print("*");
-//            } else {
-//                System.out.print(" ");
-//            }
-
-//            for (int b = 0; b < a; b++) {
-//                if ((b%2)==0) {
-//                    System.out.print("*");
-//                } else {
-//                    System.out.print(" ");
-                }
-
-//            for (int b = 0; b < a; b++) {
-//                System.out.print(b % 2 == 0 ? "*" : " ");
-//            }
-//                System.out.print("\n");
-//        }
-//    }
-
-
-//        System.out.print("*");
-//        System.out.print(" ");
-//        System.out.print("\n");
 
 
